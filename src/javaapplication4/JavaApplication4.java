36b0ffd2b93a86264cd607954fe1194f19c2c71a
Mical Pinedo <Mical Pinedo@LAPTOP-E124BRPN>
2025-01-21 20:06:55 -0500

    carga inicial

---
+++ src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+
+/**
+ *
+ * @author Mical Pinedo
+ */
+import java.util.Scanner;
+public class JavaApplication4 {
+
+    public static void main(String[] args) {
+        // Declarar variable
+        int n1,n2,suma;
+        Scanner lectura = new Scanner (System.in);
+        System.out.print("Ingresar Numero 1:");
+        n1=lectura.nextInt();
+        System.out.print("Ingresar Numero 2:");
+        n2=lectura.nextInt();
+        //proceso de datos 
+        suma=n1+n2;
+        //Salida de datos
+        System.out.print("La suma de los nuúmeroses:"+suma);
+    }
+    
+}
